799e2caff27ae7de12d61a467c924289c6790a87
saintkim12 <saintkim1232@gmail.com>
2026-08-29 11:29:08 +0900

    feat: 개인화 작업

---
+++ config.js
@@ -0,0 +1,11 @@
+window.PAGE_CONFIG = Object.freeze({
+  /**
+   * Configuration goes here
+   */
+  GITHUB_ISSUES_LINK:
+    // Format: https://api.github.com/repos/{owner}/{repo}/issues/
+    "https://api.github.com/repos/saintkim12/labellink/issues/",
+  VIEWER_BASE_URL:
+    "https://script.google.com/macros/s/AKfycbxubyYuN3r7lOzgoNRwJ7za0NI1rBLt0oSJ8rJJswh7kthKmZoNgngcI_VbKmeXW_XU/exec",
+  VIEWER_PREFIX: "$VIEWER/",
+});
